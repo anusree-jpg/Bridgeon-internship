31ce4ac3d59147f1ed8fca96aa6ff3a7fc4a96cb
anusree-jpg <anusreegopidas@gmail.com>
2026-05-27 10:49:46 +0530

    Add files via upload

--- marks.py
+++ marks.py
@@ -1,2 +1,13 @@
 students_marks=[12,34,56,45,32,59,45,23,44]
 total=sum(students_marks)
+print("total marks:",total)
+average=sum(students_marks)/len(students_marks)
+print("average marks:",average)
+highest=max(students_marks)
+print("highest marks:",highest)
+lowest=min(students_marks)
+print("lowest marks:",lowest)
+unique=list(set(students_marks))
+print("unique marks:",unique)
+above=[i for i in students_marks if i>average]
+print("above averge marks:",above)
